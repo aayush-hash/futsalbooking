9a383e4efe5262be311e8c6c9c30f549740f6eab
Aayush Kunwar <141144348+aayush-hash@users.noreply.github.com>
2024-04-29 14:38:58 +0545

    Add files via upload

--- index.php
+++ index.php
@@ -1,80 +1,104 @@
-<!DOCTYPE html>
-<html lang="en" class="" style="height: auto;">
-<?php require_once('config.php'); ?>
- <?php require_once('inc/header.php') ?>
-  <body class="hold-transition layout-top-nav" >
-     <?php $page = isset($_GET['page']) ? $_GET['page'] : 'portal';  ?>
-     <?php require_once('inc/topBarNav.php') ?>
-     <?php 
-        if(!file_exists($page.".php") && !is_dir($page)){
-            include '404.html';
-        }else{
-          if(is_dir($page))
-            include $page.'/index.php';
-          else
-            include $page.'.php';
+<?php 
+$user = $conn->query("SELECT * FROM users where id ='".$_settings->userdata('id')."'");
+foreach($user->fetch_array() as $k =>$v){
+	$meta[$k] = $v;
+}
+?>
+<?php if($_settings->chk_flashdata('success')): ?>
+<script>
+	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
+</script>
+<?php endif;?>
+<div class="card card-outline card-primary">
+	<div class="card-body">
+		<div class="container-fluid">
+			<div id="msg"></div>
+			<form action="" id="manage-user">	
+				<input type="hidden" name="id" value="<?php echo $_settings->userdata('id') ?>">
+				<div class="form-group">
+					<label for="name">First Name</label>
+					<input type="text" name="firstname" id="firstname" class="form-control" value="<?php echo isset($meta['firstname']) ? $meta['firstname']: '' ?>" required>
+				</div>
+				<div class="form-group">
+					<label for="name">Last Name</label>
+					<input type="text" name="lastname" id="lastname" class="form-control" value="<?php echo isset($meta['lastname']) ? $meta['lastname']: '' ?>" required>
+				</div>
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input type="text" name="username" id="username" class="form-control" value="<?php echo isset($meta['username']) ? $meta['username']: '' ?>" required  autocomplete="off">
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" name="password" id="password" class="form-control" value="" autocomplete="off">
+					<small><i>Leave this blank if you dont want to change the password.</i></small>
+				</div>
+				<div class="form-group">
+					<label for="" class="control-label">Avatar</label>
+					<div class="custom-file">
+		              <input type="file" class="custom-file-input rounded-circle" id="customFile" name="img" onchange="displayImg(this,$(this))">
+		              <label class="custom-file-label" for="customFile">Choose file</label>
+		            </div>
+				</div>
+				<!-- <div class="form-group d-flex justify-content-center">
+					<img src="<?php echo validate_image(isset($meta['avatar']) ? $meta['avatar'] :'') ?>" alt="" id="cimg" class="img-fluid img-thumbnail">
+				</div> -->
+				<video controls class="img-fluid img-thumbnail">
+    <source src="<?php echo validate_video(isset($meta['avatar']) ? $meta['avatar'] :'') ?>" type="video/mp4">
+    <source src="<?php echo validate_video_webm(isset($meta['avatar']) ? $meta['avatar'] :'') ?>" type="video/webm">
+    <source src="<?php echo validate_video_ogg(isset($meta['avatar']) ? $meta['avatar'] :'') ?>" type="video/ogg">
+    Your browser does not support the video tag.
+</video>
 
-        }
-      ?>
-       <script>
-        $(function(){
-          if($('header.masthead').lengt <= 0)
-            $('#mainNav').addClass('navbar-shrink')
-        })
-      </script>
-      <?php require_once('inc/footer.php') ?>
-      <div class="modal fade text-dark" id="confirm_modal" role='dialog'>
-    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title">Confirmation</h5>
-      </div>
-      <div class="modal-body">
-        <div id="delete_content"></div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade text-dark rounded-0" id="uni_modal" role='dialog'>
-    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
-      <div class="modal-content rounded-0">
-        <div class="modal-header">
-        <h5 class="modal-title"></h5>
-      </div>
-      <div class="modal-body">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade text-dark" id="uni_modal_right" role='dialog'>
-    <div class="modal-dialog modal-full-height  modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span class="fa fa-arrow-right"></span>
-        </button>
-      </div>
-      <div class="modal-body">
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade text-dark" id="viewer_modal" role='dialog'>
-    <div class="modal-dialog modal-md" role="document">
-      <div class="modal-content">
-              <button type="button" class="btn-close" data-dismiss="modal"><span class="fa fa-times"></span></button>
-              <img src="" alt="">
-      </div>
-    </div>
-  </div> 
-  </body>
-</html>
+			</form>
+		</div>
+	</div>
+	<div class="card-footer">
+			<div class="col-md-12">
+				<div class="row">
+					<button class="btn btn-sm btn-primary" form="manage-user">Update</button>
+				</div>
+			</div>
+		</div>
+</div>
+<style>
+	img#cimg{
+		height: 15vh;
+		width: 15vh;
+		object-fit: cover;
+		border-radius: 100% 100%;
+	}
+</style>
+<script>
+	function displayImg(input,_this) {
+	    if (input.files && input.files[0]) {
+	        var reader = new FileReader();
+	        reader.onload = function (e) {
+	        	$('#cimg').attr('src', e.target.result);
+	        }
+
+	        reader.readAsDataURL(input.files[0]);
+	    }
+	}
+	$('#manage-user').submit(function(e){
+		e.preventDefault();
+		start_loader()
+		$.ajax({
+			url:_base_url_+'classes/Users.php?f=save',
+			data: new FormData($(this)[0]),
+		    cache: false,
+		    contentType: false,
+		    processData: false,
+		    method: 'POST',
+		    type: 'POST',
+			success:function(resp){
+				if(resp ==1){
+					location.reload()
+				}else{
+					$('#msg').html('<div class="alert alert-danger">Username already exist</div>')
+					end_loader()
+				}
+			}
+		})
+	})
+
+</script>
